1495c9a4978338b1c9bb72aeb58716e8624e6071
uhilari <uhilari@gmail.com>
2017-04-08 01:15:30 -0500

    Iniciando Medidor

---
+++ src/PruebaAcceso/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaAcceso
+{
+  class Program
+  {
+    static void Main()
+    {
+      //new Ado.ConsultaAdo().EjecutarMedicion();
+      new Dapper.ConsultaDapper().EjecutarMedicion();
+
+      Console.WriteLine();
+      Console.WriteLine("Los tiempos estan en segundos");
+      Console.WriteLine();
+      Console.WriteLine("Terminado...");
+      Console.ReadLine();
+    }
+  }
+}
